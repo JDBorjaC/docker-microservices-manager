601f9e1b374e448568c52ded44140916edd69098
JDBorjaC <jborjad@uninorte.edu.co>
2026-02-15 12:11:42 -0500

    frontend: initial setup

---
+++ frontend/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+	plugins: [react()],
+
+	server: {
+
+		//B:Listen to every IP address
+		//B:Needed so the container can reach the Host OS (via the configured port)
+		host: true,
+	}
+})
