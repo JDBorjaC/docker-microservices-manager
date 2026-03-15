3e10a0cead048b46e9345fd8dd167fb5a685e6a9
kicconsu <sicconsu009@gmail.com>
2026-03-15 10:23:28 -0500

    useStates de servicios implementados

--- frontend/src/pages/Dash.tsx
+++ frontend/src/pages/Dash.tsx
@@ -1,31 +1,64 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../styles/monitor.css'
+import type { Service } from '../models/msm_models';
+import '../styles/monitor.css';
 
 export default function Dash() {
 
     const navi = useNavigate();
 
     const [services, setServices] = useState([
-        { id: 1, name: 'Service 1', isActive: false },
-        { id: 2, name: 'Service 2', isActive: false },
-        { id: 3, name: 'Service 3', isActive: false }
+        { id: "1", name: 'Service 1', status: false },
+        { id: "2", name: 'Service 2', status: false },
+        { id: "3", name: 'Service 3', status: false }
     ]);
 
-    const toggleService = (id:number) => {
+    const toggleService = (id:string):void => {
         setServices(services.map(service => 
-            service.id === id ? { ...service, isActive: !service.isActive } : service
+            service.id === id ? { ...service, status: !service.status } : service
         ));
     };
 
-    const editService = (id:number) => {
-        console.log('Editing ', id)
-        navi('/edit')
+    const editService = async (id:string):Promise<void> => {
+        //acceder al archivo del microservicio() dentro del contenedor, para cargarlo en la pag siguiente
+        //espero que el backend me mande un Service con todos los campos definidos
+
+        //TODO: descomentar cuando el backend exista
+        /*const backResponse = await fetch("http://backend:8000/get-by-id/"+id);
+        const userCode:Service = await backResponse.json();*/
+        
+        const userCode:Service = {id:id, name:"name", status:true, code:"blahblah blah blah"}
+        
+        try {
+            localStorage.setItem('editService', JSON.stringify(userCode));
+            navi("/edit")
+        } catch (err) {
+            console.error("Algo salió mal tratando de guardar el servicio a editar:\n", err);
+        }
     };
 
-    const deleteService = (id:number) => {
-        setServices(services.filter(service => service.id !== id));
+    const deleteService = async (id:string):Promise<void> => {
+        //mandar una petición de Borrar al backend, esperar a que me devuelva la lista de servicios restantes
+        const backResponse = await fetch("http://backend:8000/delete-service/"+id, { method:'DELETE' });
+        const remainingServices:Service[] = await backResponse.json();
+
+        setServices(remainingServices);
+
     };
+
+    //Hacer un fetch al backend y parsear los contenedores que esten listeados
+    const fetchServices = async ():Promise<void> => {
+        /* TODO: hacer url una variable de entorno maybe */
+        //espero que el backend me mande un arreglo de Service, pero sin el campo de 'code' definido
+        const backResponse =  await fetch("http://backend:8000/get-services");
+        const services:Service[] = await backResponse.json();  // toca estar atento a cambios del modelo
+        setServices(services); //parsear bien el status!!!
+    }
+
+    //ON PAGELOAD: fetch services from backend
+    useEffect(():void => {
+        fetchServices()
+    }, [])
 
 
     return (
@@ -45,6 +78,7 @@ export default function Dash() {
                             {/* Cada microservicio muestra NOMBRE, BTN_ON, BTN_EDIT, BTN_DEL */}
                             <div className='monitor-list'>
 
+                                {/* Se itera sobre cada servicio recibido del backend para añadirlo a una lista */}
                                 {services.map(service => (
                                     <div key={service.id} className='monitor-item'>
                                         <div className='service-label'>
@@ -52,19 +86,21 @@ export default function Dash() {
                                             <span>https://www.test.com.co/sumar-microservicio/pyth</span>
                                         </div>
                                         <div className='monitor-item-buttons'>
-                                            <button className='monitor-button' onClick={() => toggleService(service.id)}>{service.isActive ? 'Turn Off' : 'Turn On'}</button>
+                                            <button className='monitor-button' onClick={() => toggleService(service.id)}>{service.status ? 'Turn Off' : 'Turn On'}</button>
                                             <button className='monitor-button' onClick={() => editService(service.id)}>Edit</button>
                                             <button className='monitor-button' onClick={() => deleteService(service.id)}>Delete</button>
                                         </div>
                                     </div>
                                 ))}
+
                                 {/* BTN_CREATE MICROSERVICIO */}
                                 <div className='create-service-div'>
-                                    <button className='monitor-button' onClick={() => editService(-1337)}>
+                                    <button className='monitor-button' onClick={() => navi("/edit")}>
                                         CREAR MICROSERVICIO
                                     </button>
                                 </div>
                                 
+
                             </div>
 
                         </div>
@@ -72,8 +108,8 @@ export default function Dash() {
                 </div>
             </div>
         </div>
+        {/*--- fin de terminal de microservicios ---*/}
 
-        {/*--- COMPONENT END ---*/}
         </div>
     )
 }
--- frontend/src/pages/ServiceEditor.tsx
+++ frontend/src/pages/ServiceEditor.tsx
@@ -1,4 +1,26 @@
+import { useEffect, useState } from 'react';
+import type { Service } from '../models/msm_models'
+
+
+
 export default function ServiceEditor(){
+
+    const [editable, setEditable] = useState(():Service => {
+        //cargar editService si es que está
+        const e = localStorage.getItem("editService");
+        console.log(e)
+        return e ? JSON.parse(e):{
+            id:'',
+            name:'',
+            status:false,
+            code:''
+        }
+    });
+
+    //para mandar MS: nombre, desc?, lenguage y codigo
+
+    // Si se carga un servicio, colocar su código en el textArea
+
     
     return (
         <div className="micro-editor">
@@ -19,9 +41,13 @@ export default function ServiceEditor(){
                             <p>
                                 Recomendación: Pegar el código desde algún Sandbox en linea del lenguaje seleccionado.
                             </p>
+                            <p>
+                                Advertencia: Para que su microservicio funcione, tiene que definir una función 'microservice()', que actuará como la función principal que será ejecutada. 
+                            </p>
                             
                             <form className="service-form" action="/submit-service" method="POST">
-                                <textarea className="code-input" name="code" placeholder="Placeholder tiene que ser el boilerplate de cada lenguage"/>
+                                {/* El textarea carga de local-storage cada page-load. es decir, refrescar la página supone perder cambios. Para el scope del proyecto no importa mucho.*/}
+                                <textarea className="code-input" name="code" placeholder="¡Copiar y pegar código aquí!">{editable.code}</textarea>
                                 <div>
                                     <input className="monitor-button" type="submit" value="MANDAR"/>
                                     <select className="drop-down-menu" name="lang-select-menu">
